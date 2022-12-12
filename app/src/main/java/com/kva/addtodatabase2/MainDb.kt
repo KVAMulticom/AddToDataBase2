2fe13769d55f6111a014da2452faf07f25ca0e2c
KVAMulticom <kva-09@mail.ru>
2022-12-12 14:11:25 +0300

    Initial commit

---
+++ app/src/main/java/com/kva/addtodatabase2/MainDb.kt
@@ -0,0 +1,23 @@
+package com.kva.addtodatabase2
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Item::class], version = 1)  //анотация датабазы и в нее наш созданный Entity
+                                    // который мы назвали Item и указываем версию
+abstract class MainDb:RoomDatabase() {
+    abstract fun getDao(): Dao
+
+    companion object {
+        fun getDb(context:Context):MainDb{
+            return Room.databaseBuilder(  //создание базы данных
+                context.applicationContext,  //
+                MainDb::class.java,
+                  "test_db"  //название базы данных
+            ).build()  //строит
+
+        }
+    }
+}
